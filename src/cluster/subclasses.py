4ebb730d1f8efc9a68e57429b38d72a9342f4356
Dmitri Nikonov <nikonov@volleyball-it.de>
2019-05-02 14:30:09 +0200

    Further work

--- src/cluster/subclasses.py
+++ src/cluster/subclasses.py
@@ -2,21 +2,31 @@ from docutils.nodes import label, General
 
 from data import DATAP, load_articledict, save_articledict,start_time, stop_time
 from data.explore.feature_freq import analyze_feature_frequency
-from pandas import DataFrame, Series
+import pandas as pd
+from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA, FastICA
-from sklearn.cluster import AgglomerativeClustering, SpectralClustering
+from sklearn.cluster import AgglomerativeClustering, SpectralClustering, KMeans, DBSCAN
 from sklearn.mixture import GaussianMixture
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+from skfeature.function.similarity_based import SPEC, lap_score
 
 # %matplotlib inline
 
-FEATURE_NAMES = ["DbpediaInfoboxTemplate", "Lemmas", "URL_Braces_Words", "COPHypernym", "Wikipedia_Lists"]  # Lemmas,
-N_CLUSTERS = 2
+FEATURE_NAMES = [
+    "DbpediaInfoboxTemplate",
+    "Lemmas",
+    "URL_Braces_Words",
+    "COPHypernym",
+    "Wikipedia_Lists"
+]
+N_CLUSTERS = 5
+N_MOST_IMPORTANT_FEATURES = 1000
 
 def build_feature_vector(articles):
-    features_freqs = analyze_feature_frequency(articles, FEATURE_NAMES)
+    features_freqs = analyze_feature_frequency(articles, F_SetNames=FEATURE_NAMES)
     # print(features_freqs)
     return features_freqs.keys()
 
@@ -25,7 +35,7 @@ def build_feature_matrix(features, articles):
     feature_matrix = {f: [] for f in features}
     for a in articles.items():
         # print(a)
-        feature_freq = analyze_feature_frequency({a[0]: a[1]}, FEATURE_NAMES)
+        feature_freq = analyze_feature_frequency({a[0]: a[1]}, F_SetNames=FEATURE_NAMES)
         for key in feature_matrix:
             feature_matrix[key].append(1 if key in feature_freq else 0)
     return feature_matrix
@@ -37,37 +47,105 @@ def create_dataframe(articles):
     feature_matrix = build_feature_matrix(features, articles)
     feature_matrix["Name"] = [a for a in articles]
 
-    df = DataFrame(feature_matrix, columns=["Name"] + list(features))
+    df = pd.DataFrame(feature_matrix, columns=["Name"] + list(features))
     df = df.set_index("Name")
     return df
 
 
-def plot_reduced(df, n_clusters, decompositer = PCA):
-    reduced = df
-    print("Plotting two-dimensional dataframe reduction...")
-    # reduced = DataFrame(decompositer(n_components=2).fit_transform(df.iloc[:, 0:-1]))
-    # reduced = reduced.assign(Class=Series(df.loc[:, "Class"].values, index=reduced.index))
-    # print(reduced)
+def plot_2d(df, n_clusters):
+    # reduced = df
+    print("Plotting 2D dataframe reduction...")
+    reduced = pd.DataFrame(PCA(n_components=2).fit_transform(df.iloc[:, 0:-1]))
+    reduced = reduced.assign(Class=pd.Series(df.loc[:, "Class"].values, index=reduced.index))
+
     for n in range(n_clusters):
-        plt.scatter(reduced[reduced["Class"] == n].iloc[:, 0], reduced[reduced["Class"] == n].iloc[:, 1], cmap=plt.get_cmap("Spectral"), label= "Class %s" % n)
+        plt.scatter(reduced[reduced["Class"] == n].iloc[:, 0], reduced[reduced["Class"] == n].iloc[:, 1],  cmap=plt.get_cmap("Spectral"), label="Class %s" % n)
     plt.legend()
     plt.show()
 
 
-def analyze_clusters(ad, df, n_clusters):
-    print("Analyzing clusters...")
+def plot_3d(df, n_clusters):
+    # reduced = df
+    print("Plotting 3D dataframe reduction...")
+    reduced = pd.DataFrame(PCA(n_components=3).fit_transform(df.iloc[:, 0:-1]))
+    reduced = reduced.assign(Class=pd.Series(df.loc[:, "Class"].values, index=reduced.index))
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
     for n in range(n_clusters):
-        analyze_cluster(ad, df[df["Class"] == n])
+        ax.scatter(reduced[reduced["Class"] == n].iloc[:, 0], reduced[reduced["Class"] == n].iloc[:, 1], reduced[reduced["Class"] == n].iloc[:, 2], cmap=plt.get_cmap("Spectral"), label="Class %s" % n)
+    plt.legend()
+    plt.show()
+
+def explore_silhouette(df, n_clusters):
+    silhouettes = []
+    for n in range(2, n_clusters, 1):
+        clustered = create_cluster(reduced_df, n)
+        silhouette = cluster_scores(clustered)
+        silhouettes.append((n, silhouette))
+
+    xs = [s[0] for s in silhouettes]
+    ys = [s[1] for s in silhouettes]
+    plt.plot(xs, ys, '-bX', markevery=True)
+    plt.show()
+
+def analyze_clusters(ad, df, n_clusters, relevant_features):
+    print("Analyzing clusters...")
+    # print("Relevant features: %s" % str(relevant_features))
+    for n in range(n_clusters):
+        analyze_cluster(ad, df[df["Class"] == n], relevant_features)
 
 
-def analyze_cluster(ad, cluster):
+def analyze_cluster(ad, cluster, relevant_features=None):
     article_names = cluster.index.values
 
-    print("Relevant features: %s" % str(cluster.columns.values))
-
     articles = dict([a for a in ad.items() if a[0] in article_names])
+    print("%s articles clustered in cluster %s" % (len(articles), cluster.loc[:, "Class"][0]))
     print("Feature Frequency for cluster %s" % cluster.loc[:, "Class"][0])
-    print(dict(sorted(analyze_feature_frequency(articles, F_SetNames=FEATURE_NAMES).items(), key=lambda v: v[1], reverse=True)))
+    if relevant_features is not None:
+        print(dict(sorted(analyze_feature_frequency(articles, relevant_features=relevant_features).items(), key=lambda v: v[1], reverse=True)))
+
+
+def calc_feature_importance(data):
+    print("Calculating feature importance matrix...")
+    X = SPEC.spec(data.values, style=0)
+    # X = SPEC.feature_ranking(X, style=0)
+    d_spec = pd.DataFrame([X], columns=data.columns.values, index=["SPEC"])
+    d_spec = d_spec.sort_values("SPEC", axis=1, ascending=False)
+    d_spec.to_csv(DATAP + "/cluster/seed_SPEC.csv")
+
+    return d_spec
+
+
+def load_feature_importance_matrix():
+    print("Loading feature importance matrix...")
+    matrix = pd.read_csv(DATAP + "/cluster/seed_SPEC.csv")
+    dropped_columns = [c for c in matrix.columns.values[1:] if c.split("::")[0] not in FEATURE_NAMES]
+    matrix = matrix.drop(labels=dropped_columns, axis=1)
+
+    return matrix
+
+
+def slice_n_relevant_features(data, feature_importance, n):
+    print("Selecting the %s most relevant features..." % str(n))
+    print(data.shape)
+    print(feature_importance.shape)
+    relevant_columns = feature_importance.iloc[0, 0:n+1].index.values[1:]
+    return data.loc[:, relevant_columns]
+
+
+def create_cluster(data, n):
+    X = data
+    y = pd.Series(AgglomerativeClustering(n_clusters=n).fit_predict(X), index=X.index)
+
+    data = data.assign(Class=y, index=data.index)
+    return data
+
+
+def cluster_scores(data):
+    silhouette = silhouette_score(data.iloc[:, 1:-1], labels=data["Class"].values)
+    return silhouette
 
 
 if __name__ == "__main__":
@@ -78,22 +156,25 @@ if __name__ == "__main__":
     # print(seed)
     df = create_dataframe(seed)
 
-    corr = df.corr()
+    # calc_feature_importance(df)
+    feature_importance = load_feature_importance_matrix()
+    # feature_importance = calc_feature_importance(df)
 
-    f, ax = plt.subplots(figsize=(10, 6))
-    hm = sns.heatmap(round(corr, 2), annot=True, ax=ax, cmap="coolwarm", fmt='.2f',
-                     linewidths=.05)
-    f.subplots_adjust(top=0.93)
-    t = f.suptitle('Correlation Heatmap', fontsize=14)
+    reduced_df = slice_n_relevant_features(df, feature_importance, n=N_MOST_IMPORTANT_FEATURES)
 
-    plt.show()
+    # print(reduced_df.shape)
 
-    # print("Selecting features...")
-    # k_select = SelectKBest(score_func=chi2, k=100)
-    # df = k_select.fit(df)
-    # print("Applying FastICA to seed...")
-    # pca = PCA(n_components=50)
-    # df = DataFrame(pca.fit_transform(df), index=df.index)
+    explore_silhouette(reduced_df, n_clusters=30)
+
+
+
+
+    # plot_2d(clustered, N_CLUSTERS)
+    # analyze_clusters(ad, clustered, N_CLUSTERS, relevant_features=[c for c in feature_importance.columns.values[1:] if "COPHypernym" in c])
+
+
+
+    # clustered["Class"].to_csv(DATAP + "/cluster/clusters.csv")
     #
     # print("Creating cluster...")
     # ac = GaussianMixture(n_components=N_CLUSTERS)
--- src/data/explore/feature_freq.py
+++ src/data/explore/feature_freq.py
@@ -1,8 +1,17 @@
 from data import load_articledict
 
 
-def analyze_feature_frequency(ad, F_SetNames):
+def analyze_feature_frequency(ad, **kwargs):
     freq = dict()
+
+    if "F_SetNames" not in kwargs:
+        kwargs["F_SetNames"] = []
+    F_SetNames = kwargs["F_SetNames"]
+
+    if "relevant_features" not in kwargs:
+        kwargs["relevant_features"] = []
+    relevant_features = kwargs["relevant_features"]
+
     for a in ad:
         for F_Name in F_SetNames:
             if F_Name not in ad[a]:
@@ -11,4 +20,13 @@ def analyze_feature_frequency(ad, F_SetNames):
                 if F_Name + "::" + f not in freq:
                     freq[F_Name + "::" + f] = 0
                 freq[F_Name + "::" + f] += 1
+
+        for feature in relevant_features:
+            F_SetName, F_Name = feature.split("::")
+            if F_SetName not in ad[a]:
+                continue
+            if F_Name in ad[a][F_SetName]:
+                if feature not in freq:
+                    freq[feature] = 0
+                freq[feature] += 1
     return freq
